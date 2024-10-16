d8bfb176148d52df00cee3f92f82c7c8d8d581e4
ezgaro <ezgaro2005@gmail.com>
2024-10-16 09:13:14 +0300

    Implementing Smooth Scrolling

--- OmniFood/js/script.js
+++ OmniFood/js/script.js
@@ -1,14 +1,38 @@
 ///////////////////////////////////////////////////////////
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.header');
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', () => {
-  headerEl.classList.toggle('nav-open');
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
 });
 
+const allLinks = document.querySelectorAll("a:link");
 
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionElement = document.querySelector(href);
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
 
+    if(link.classList.contains('main-nav-link')){
+      headerEl.classList.toggle("nav-open");
+
+    }
+
+  });
+});
 
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
